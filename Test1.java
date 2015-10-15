76470ed07e2e949002b9c205277e26aa10f37fc4
priyavasanth <priya24ster@gmail.com>
2015-10-14 23:15:52 -0400

    I am committing with added change
    To the branch - new change has been added by me

--- Test1.java
+++ Test1.java
@@ -1,9 +1,9 @@
-﻿public class Test1 {
+public class Test1 {
 	public static void main(string[] args) {
 		System.out.println(“Priya has changed this file”);
 		System.out.println("Vasanth has changed this file");
 		System.out.println("third");
-		System.out.println("This change should show only in the branch");
+		System.out.println("This change should show only in the branch one”);
 	}
 	
 }
